f85bcc3dce0201a788a1d862f349f441853d6d57
ahmed almeslmy <ahmedhiaro@gmail.com>
2026-06-18 18:45:06 +0300

    gffghghf dssdsd b :wq with '#' will be ignored, and an empty message aborts the commit.

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-    timeout: 90 * 1000,
+    timeout: 150 * 1000,
 
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 8 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
